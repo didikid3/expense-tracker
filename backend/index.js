ef8f2bc47240f3ee5af976882ebac725b4e31252
Brandon Chao <34197234+didikid3@users.noreply.github.com>
2024-03-18 16:17:23 -0700

    integrate ngrok and nodemon for auto refresh of backend

--- backend/index.js
+++ backend/index.js
@@ -4,5 +4,8 @@ const PORT = process.env.PORT || 3000
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello"))
+app.get("/api", (req, res) => {
+    res.json({"users": ["user1", "user2", "user3"]})
+})
 
 app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
