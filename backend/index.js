1bb5819bc6b5087cc385f9aa6886382ead7e95bc
Brandon Chao <34197234+didikid3@users.noreply.github.com>
2024-03-22 23:51:30 -0700

    Added CORS to backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,16 @@
-const express = require("express");
+const express = require('express')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 const app = express();
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
 
 app.get("/", (req, res) => res.send("Hello"))
 app.get("/api", (req, res) => {
     res.json({"users": ["user1", "user2", "user3"]})
 })
 
-app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`))
